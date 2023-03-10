b7e7e6ce9b30c3c378e569c8ad16563448a12f5a
paulrumin <pasha@jedo.localdomain>
2023-03-10 15:15:54 -0800

    Step 1

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -35,6 +35,11 @@ const Card = (data) => {
             <span class="image__photog">${imgData.user.name}</span>.
           </p>
           <p>
+            Uploaded on
+            <time class="image__date" datetime="">${imgData.created_at}</time>
+
+          </p>
+          <p>
             <a href="${imgData.links.self}" class="image__link">
               View it on Unsplash.
             </a>
